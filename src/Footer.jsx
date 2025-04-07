6664c6f3e3bb9ec5825a0b3f4989ce086390b1f2
Katei <kateievans28@gmail.com>
2025-04-07 12:05:41 +0300

    first-react

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return(
+        <div>
+            <footer>
+                <h1>This is footer</h1>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar (){
+    return(
+        <div className="">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
